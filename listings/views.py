7ebd9948c94154a99fcd5ce0007289aad1c30429
DominatorProgramator <st49522@student.upce.cz>
2019-02-02 11:16:01 +0100

    Add search page and serach queries

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Listing
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 # Create your views here.
@@ -30,4 +31,44 @@ def listing(req, listing_id):
 
 
 def search(req):
-    return render(req, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Keywords
+    if 'keywords' in req.GET:
+        keywords = req.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in req.GET:
+        city = req.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    if 'state' in req.GET:
+        state = req.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
+    # Bedroom
+    if 'bedrooms' in req.GET:
+        bedrooms = req.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if 'price' in req.GET:
+        price = req.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_list,
+        'values': req.GET
+    }
+
+    return render(req, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 # Create your views here.
@@ -9,7 +10,10 @@ def index(req):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
 
     context = {
-        'listings': listings
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
     }
 
     return render(req, 'pages/index.html', context)
@@ -26,4 +30,3 @@ def about(req):
     }
 
     return render(req, 'pages/about.html', context)
-
